7797867d9d55355b7d3bf975fcbcc898f0b4ffc1
Ilker Duymaz <ilkrdymz@gmail.com>
2025-06-15 16:39:12 +0200

    monitortools

--- src/monitortools.py
+++ src/monitortools.py
@@ -16,30 +16,14 @@ class MonitorTools:
         else:
             raise NotImplementedError("Unsupported OS")
 
-    def get_available_modes(self):
-        if self.system == "Windows":
-            raise NotImplementedError(
-                "Fetching available modes is not implemented for Windows."
-            )
-        elif self.system == "Linux":
-            return self._get_linux_modes()
-        else:
-            raise NotImplementedError("Unsupported OS")
-
     def set_settings(self, resolution=None, refresh_rate=None):
-        available_modes = self.get_available_modes()
         if resolution and refresh_rate:
-            if (resolution, str(refresh_rate)) not in available_modes:
-                raise ValueError(
-                    f"Requested mode {resolution}@{refresh_rate}Hz is not supported. Available modes: {available_modes}"
-                )
-
-        if self.system == "Windows":
-            return self._set_windows_settings(resolution, refresh_rate)
-        elif self.system == "Linux":
-            return self._set_linux_settings(resolution, refresh_rate)
-        else:
-            raise NotImplementedError("Unsupported OS")
+            if self.system == "Windows":
+                return self._set_windows_settings(resolution, refresh_rate)
+            elif self.system == "Linux":
+                return self._set_linux_settings(resolution, refresh_rate)
+            else:
+                raise NotImplementedError("Unsupported OS")
 
     # Windows methods
     def _get_windows_settings(self):
@@ -48,11 +32,12 @@ class MonitorTools:
             dxinfo_file = "dxinfo.txt"
             subprocess.run(f"dxdiag /t {dxinfo_file}", shell=True, check=True)
             with open(
-                dxinfo_file, "r", encoding="utf-16" if os.name == "nt" else "utf-8"
+                dxinfo_file, "r", encoding="Windows-1252" if os.name == "nt" else "utf-8"
             ) as f:
                 content = f.read()
+            
             os.remove(dxinfo_file)
-            match = re.search(r"Current Mode:\s*(\d+x\d+) \(.*?\) \((\d+)Hz\)", content)
+            match = re.search(r"Current Mode:\s*(\d+\s*x\s*\d+) \(.*?\) \((\d+)Hz\)", content)
             if match:
                 resolution = match.group(1)
                 refresh_rate = match.group(2)
@@ -89,6 +74,7 @@ class MonitorTools:
             if resolution:
                 res = resolution.lower().split("x")
                 color_depth = 32
+                # cmd = f"nircmd.exe setdisplay monitor:0 {res[0]} {res[1]} {color_depth} {refresh_rate}" # syntax for multiple monitors
                 cmd = f"nircmd.exe setdisplay {res[0]} {res[1]} {color_depth} {refresh_rate}"
                 subprocess.run(cmd, shell=True, check=True)
             return {"status": "Applied using NirCmd"}
@@ -135,3 +121,9 @@ class MonitorTools:
             return {"status": "Settings changed"}
         except Exception as e:
             return {"error": str(e)}
+
+if __name__ == "__main__":
+    mt = MonitorTools()
+    print(mt.get_settings())
+    mt.set_settings("2560x1440", "60")
+    print(mt.get_settings())
